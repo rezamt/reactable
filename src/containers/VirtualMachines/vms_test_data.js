ec5187a442d827c65f786f6ecb212aa878eca6aa
reza <rezamt@gmail.com>
2018-07-17 00:48:27 +1000

    Genesis Block

---
+++ src/containers/VirtualMachines/vms_test_data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        "id": "",
+        "size": "",
+        "name" :"",
+        "networkInterface": [
+            {
+                "privateIp" : "",
+                "privateIPType" : "",
+                "publicIp" : "",
+                "publicIpType" : "",
+            }
+        ],
+        "location" :"",
+        "subscription" : "",
+        "resourceGroup" : "",
+        "tags" : "",
+        "imagePublisher": "",
+        "provisioningState" : "succeeded",
+        "powerState" : "running"
+    }
+];
